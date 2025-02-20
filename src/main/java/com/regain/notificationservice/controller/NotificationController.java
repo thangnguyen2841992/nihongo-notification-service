f98a0495ffb0d34a6c34f06ebd494a979ca69c01
thangnguyen1992 <ntthang4@cmcglobal.vn>
2025-02-20 15:49:59 +0700

    ....

--- src/main/java/com/regain/notificationservice/controller/NotificationController.java
+++ src/main/java/com/regain/notificationservice/controller/NotificationController.java
@@ -3,10 +3,7 @@ package com.regain.notificationservice.controller;
 import com.regain.notificationservice.model.MessageDTO;
 import com.regain.notificationservice.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
